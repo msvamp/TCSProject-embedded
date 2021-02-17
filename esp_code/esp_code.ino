9042b400e48847b2dbd428ff561bb85b3f550f39
Manas Sambhus <51392775+msvamp@users.noreply.github.com>
2021-02-17 08:59:53 +0530

    Cosmetic changes (Indentation fix)

--- esp_code/esp_code.ino
+++ esp_code/esp_code.ino
@@ -19,150 +19,147 @@ static auto hiRes = esp32cam::Resolution::find(800, 600); // set the resolution 
 
 void handleBmp() // A function to retrieve BMP images contiuosly from the server
 {
-  if (!esp32cam::Camera.changeResolution(loRes)) {
-    Serial.println("SET-LO-RES FAIL");
-  }
+	if (!esp32cam::Camera.changeResolution(loRes)) {
+		Serial.println("SET-LO-RES FAIL");
+	}
 
-  auto frame = esp32cam::capture();
-  if (frame == nullptr) {
-    Serial.println("CAPTURE FAIL");
-    server.send(503, "", "");
-    return;
-  }
-  Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
-                static_cast<int>(frame->size()));
+	auto frame = esp32cam::capture();
+	if (frame == nullptr) {
+		Serial.println("CAPTURE FAIL");
+		server.send(503, "", "");
+		return;
+	}
+	Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
+					static_cast<int>(frame->size()));
 
-  if (!frame->toBmp()) {
-    Serial.println("CONVERT FAIL");
-    server.send(503, "", "");
-    return;
-  }
-  Serial.printf("CONVERT OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
-                static_cast<int>(frame->size()));
+	if (!frame->toBmp()) {
+		Serial.println("CONVERT FAIL");
+		server.send(503, "", "");
+		return;
+	}
+	Serial.printf("CONVERT OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
+					static_cast<int>(frame->size()));
 
-  server.setContentLength(frame->size());
-  server.send(200, "image/bmp"); // sending the image 
-  WiFiClient client = server.client();
-  frame->writeTo(client);
+	server.setContentLength(frame->size());
+	server.send(200, "image/bmp"); // sending the image
+	WiFiClient client = server.client();
+	frame->writeTo(client);
 }
 
 void serveJpg() // A function to retrieve JPG images contiuosly from the server
 {
-  auto frame = esp32cam::capture();
-  if (frame == nullptr) {
-    Serial.println("CAPTURE FAIL");
-    server.send(503, "", "");
-    return;
-  }
-  Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
-                static_cast<int>(frame->size()));
+	auto frame = esp32cam::capture();
+	if (frame == nullptr) {
+		Serial.println("CAPTURE FAIL");
+		server.send(503, "", "");
+		return;
+	}
+	Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
+					static_cast<int>(frame->size()));
 
-  server.setContentLength(frame->size());
-  server.send(200, "image/jpeg"); //sending the image
-  WiFiClient client = server.client();
-  frame->writeTo(client);
+	server.setContentLength(frame->size());
+	server.send(200, "image/jpeg"); //sending the image
+	WiFiClient client = server.client();
+	frame->writeTo(client);
 }
 
-void handleJpgLo()  // A function to handle low quality JPG images contiuosly from the server
+void handleJpgLo()	// A function to handle low quality JPG images contiuosly from the server
 {
-  if (!esp32cam::Camera.changeResolution(loRes)) {
-    Serial.println("SET-LO-RES FAIL");
-  }
-  serveJpg();
+	if (!esp32cam::Camera.changeResolution(loRes)) {
+		Serial.println("SET-LO-RES FAIL");
+	}
+	serveJpg();
 }
 
-void handleJpgHi()  // A function to handle High quality JPG images contiuosly from the server
+void handleJpgHi()	// A function to handle High quality JPG images contiuosly from the server
 {
-  if (!esp32cam::Camera.changeResolution(hiRes)) {
-    Serial.println("SET-HI-RES FAIL");
-  }
-  serveJpg();
+	if (!esp32cam::Camera.changeResolution(hiRes)) {
+		Serial.println("SET-HI-RES FAIL");
+	}
+	serveJpg();
 }
 
-void handleJpg()  // A function to rhandle JPG images contiuosly from the server
+void handleJpg()	// A function to rhandle JPG images contiuosly from the server
 {
-  server.sendHeader("Location", "/cam-hi.jpg");
-  server.send(302, "", "");
+	server.sendHeader("Location", "/cam-hi.jpg");
+	server.send(302, "", "");
 }
 
-void handleMjpeg()  // A function to video stream of JPEG images  contiuosly from the server
+void handleMjpeg()	// A function to video stream of JPEG images	contiuosly from the server
 {
-  if (!esp32cam::Camera.changeResolution(hiRes)) {
-    Serial.println("SET-HI-RES FAIL");
-  }
+	if (!esp32cam::Camera.changeResolution(hiRes)) {
+		Serial.println("SET-HI-RES FAIL");
+	}
 
-  Serial.println("STREAM BEGIN");
-  WiFiClient client = server.client();
-  auto startTime = millis();
-  int res = esp32cam::Camera.streamMjpeg(client);
-  if (res <= 0) {
-    Serial.printf("STREAM ERROR %d\n", res);
-    return;
-  }
-  auto duration = millis() - startTime;
-  Serial.printf("STREAM END %dfrm %0.2ffps\n", res, 1000.0 * res / duration);
+	Serial.println("STREAM BEGIN");
+	WiFiClient client = server.client();
+	auto startTime = millis();
+	int res = esp32cam::Camera.streamMjpeg(client);
+	if (res <= 0) {
+		Serial.printf("STREAM ERROR %d\n", res);
+		return;
+	}
+	auto duration = millis() - startTime;
+	Serial.printf("STREAM END %dfrm %0.2ffps\n", res, 1000.0 * res / duration);
 }
 
 const int GPIO_PIN_NUMBER_15 = 15;
 
 void signal_on(){
-    digitalWrite(GPIO_PIN_NUMBER_15, HIGH); 
-    server.send(200, "signal_on");
+	digitalWrite(GPIO_PIN_NUMBER_15, HIGH);
+	server.send(200, "signal_on");
 }
 
 void signal_off(){
-    digitalWrite(GPIO_PIN_NUMBER_15, LOW);
-    server.send(200, "signal_off");
+	digitalWrite(GPIO_PIN_NUMBER_15, LOW);
+	server.send(200, "signal_off");
 }
-
 
 void setup()
 {
-  Serial.begin(115200); // begin server at baud rate 115200
-  pinMode(GPIO_PIN_NUMBER_15,OUTPUT);
-  digitalWrite(GPIO_PIN_NUMBER_15,HIGH);
-  
-  Serial.println();
+	Serial.begin(115200); // begin server at baud rate 115200
+	pinMode(GPIO_PIN_NUMBER_15,OUTPUT);
+	digitalWrite(GPIO_PIN_NUMBER_15,HIGH);
 
-  using namespace esp32cam;
-  Config cfg;
-  cfg.setPins(pins::AiThinker);
-  cfg.setResolution(hiRes);
-  cfg.setBufferCount(2);
-  cfg.setJpeg(80);
+	using namespace esp32cam;
+	Config cfg;
+	cfg.setPins(pins::AiThinker);
+	cfg.setResolution(hiRes);
+	cfg.setBufferCount(2);
+	cfg.setJpeg(80);
 
-  bool ok = Camera.begin(cfg);
-  Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
+	bool ok = Camera.begin(cfg);
+	Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
 
-  WiFi.persistent(false);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASS); // connect to the WIFI network
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
+	WiFi.persistent(false);
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(WIFI_SSID, WIFI_PASS); // connect to the WIFI network
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+	}
 
-  Serial.print("http://");
-  Serial.println(WiFi.localIP());
-  
+	Serial.print("http://");
+	Serial.println(WiFi.localIP());
+
 // below lines of code start the servers for all the described function.
-  server.on("/cam.bmp", handleBmp);
-  server.on("/cam-lo.jpg", handleJpgLo);
-  server.on("/cam-hi.jpg", handleJpgHi);
-  server.on("/cam.jpg", handleJpg);
-  server.on("/cam.mjpeg", handleMjpeg);
-  server.on("/signal_on",signal_on);
-  server.on("/signal_off",signal_off);
+	server.on("/cam.bmp", handleBmp);
+	server.on("/cam-lo.jpg", handleJpgLo);
+	server.on("/cam-hi.jpg", handleJpgHi);
+	server.on("/cam.jpg", handleJpg);
+	server.on("/cam.mjpeg", handleMjpeg);
+	server.on("/signal_on",signal_on);
+	server.on("/signal_off",signal_off);
 
-  server.begin();
+	server.begin();
 
 // these lines of code are used only for debugging purposes to verify weather or not the cam is connected to server
-  Serial.println("  /cam.bmp");
-  Serial.println("  /cam-lo.jpg");
-  Serial.println("  /cam-hi.jpg");
-  Serial.println("  /cam.mjpeg");
+	Serial.println("	/cam.bmp");
+	Serial.println("	/cam-lo.jpg");
+	Serial.println("	/cam-hi.jpg");
+	Serial.println("	/cam.mjpeg");
 }
 
 void loop()
 {
-  server.handleClient();
+	server.handleClient();
 }
